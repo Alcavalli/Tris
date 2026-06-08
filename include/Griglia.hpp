0ccaed0e4b5cb06d8be396071de9e0e33b0f4986
Alcavalli <alcavalli09@gmail.com>
2026-06-08 20:31:59 +0200

    fourth commit, third attempt: add methods checkWinner and isFull

--- include/Griglia.hpp
+++ include/Griglia.hpp
@@ -8,10 +8,12 @@ class Grid
 {
 private:
     std::array<std::array<CellStatus, Constants::COLS>, Constants::ROWS> griglia{};
-    //! {} inizializza tutto a zero (primo elemento, Empty) così da evitare valori spazzatura
+    //* {} inizializza tutto a zero (primo elemento, Empty) così da evitare valori spazzatura
 
 public:
     Grid();
     const CellStatus& getCell(int row, int col) const;              //! bastano 2 const
     void setCell(int row, int col, CellStatus nuovo_stato);         //! per i tipi semplici si può passare anche solo per valore (senza const&)
+    CellStatus checkWinner() const;     //* controlla se c'è un tris, se non c'è restituisce Empty
+    bool isFull() const;                //* controlla se è piena (pareggio)
 };
