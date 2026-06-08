651bc873610c1728bdf5ae49038244a3f245de7e
Alcavalli <alcavalli09@gmail.com>
2026-06-09 00:03:44 +0200

    ninth commit, second attempt: improvement and conclusion of the Renderer.hpp

--- include/Renderer.hpp
+++ include/Renderer.hpp
@@ -1,20 +1,18 @@
 #pragma once
 
-#include "Constants.hpp"
-#include "Enums.hpp"
 #include <SFML/Graphics.hpp>
-#include "Grid.hpp"
+#include "Grid.hpp"             //! Constants e Enums sono già inclusi, sarebbero ridondanti
 
 class Renderer
 {
 private:
     sf::Font font;
-    sf::Text game_over, turno, punteggio;
+    sf::Text testo_game_over, testo_turno, testo_punteggio, testo_restart;
     sf::Clock lampeggio;
     bool visibile{};
     sf::Vector2f toPixel(const sf::Vector2i& cella) const;
 
 public:
     Renderer();
-    void render(sf::RenderWindow& window, const Grid& table);
+    void render(sf::RenderWindow& window, const Grid& table, const GameStatus& stato_gioco, const CellStatus& turno_corrente);
 };
